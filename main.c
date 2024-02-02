d75b55ba9a5e3fb402ab4c6cb1a87373a1098931
supe4cookie <supe4cookie@gmail.com>
2024-02-02 02:55:07 +0100

    sort 5 finished, rewrite sort 3

--- main.c
+++ main.c
@@ -27,22 +27,37 @@ void sa(int *a, size_t size)
 	printf("sa\n");
 }
 
-
-void rra(int *a, size_t size)
+void ra(t_stack *a)
 {
-	int tmp;
-	int i;
+	size_t i;
+	int		tmp;
 
-	if (size < 2)
-		return ;
-	tmp = a[size - 1];
-	i = size - 1;
-	while (i > 0)
+	i = a->start;
+	tmp = a->stack[i];
+	while ((i + 1) < a->end)
 	{
-		a[i] = a[i - 1];
+		a->stack[i] = a->stack[i + 1];
+		i++;
+	}
+	a->stack[i] = tmp;
+	printf("ra\n");
+}
+
+void rra(t_stack *a)
+{
+	size_t	i;
+	int		tmp;
+
+	if ((a->end - a->start) < 2)
+		return ;
+	i = (a->end - a->start) - 1;
+	tmp = a->stack[i];
+	while (i > a->start)
+	{
+		a->stack[i] = a->stack[i - 1];
 		i--;
 	}
-	a[0] = tmp;
+	a->stack[a->start] = tmp;
 	printf("rra\n");
 }
 
@@ -65,22 +80,6 @@ int get_n_bit(int num, size_t n)
 	return ((num >> n) & 1);
 }
 
-void ra(t_stack *a)
-{
-	size_t i;
-	int		tmp;
-
-	i = a->start;
-	tmp = a->stack[i];
-	while ((i + 1) < a->end)
-	{
-		a->stack[i] = a->stack[i + 1];
-		i++;
-	}
-	a->stack[i] = tmp;
-	printf("ra\n");
-}
-
 void rb(t_stack *b)
 {
 	size_t i;
@@ -97,40 +96,66 @@ void rb(t_stack *b)
 	printf("rb\n");
 }
 
+int	get_min_index(t_stack *s)
+{
+	int	i;
+	int	min;
+	int	min_index;
+
+	i = s->end - 1;
+	min = s->stack[i];
+	min_index = i;
+	while (i >= (int)s->start)
+	{
+		if (s->stack[i] < min)
+		{
+			min = s->stack[i];
+			min_index = i;
+		}
+		i--;
+	}
+	return (min_index + s->start);
+}
+
 int is_sorted(t_stack *a)
 {
     size_t i;
 
-    if (a->end - a->start <= 1)  // If there's one or no element, it's sorted
+    if (a->end - a->start <= 1)
         return (1);
 
-    for (i = a->start; i < a->end - 1; i++)  // Iterate up to the second-to-last element
+    for (i = a->start; i < a->end - 1; i++)
     {
         if (a->stack[i] > a->stack[i + 1])
-            return (0);  // Return 0 if any element is greater than its next element
+            return (0);
     }
-    return (1);  // Return 1 if no such pair is found
+    return (1);
 }
 
-//void three_sort(int *a, size_t size)
-//{
-//	if (a[0] > a[1] && a[1] < a[2] && a[0] < a[2])
-//		sa(a, size);
-//	else if (a[0] > a[1] && a[1] > a[2] && a[0] > a[2])
-//	{
-//		sa(a, size);
-//		rra(a, size);
-//	}
-//	else if (a[0] > a[1] && a[1] < a[2] && a[0] > a[2])
-//		ra(a, size);
-//	else if (a[0] < a[1] && a[1] > a[2] && a[0] < a[2])
-//	{
-//		sa(a, size);
-//		ra(a, size);
-//	}
-//	else
-//		rra(a, size);
-//}
+void three_sort(t_stack *s)
+{
+	int *a;
+	size_t size;
+
+	a = s->stack;
+	size = s->end - s->start;
+	if (a[0] > a[1] && a[1] < a[2] && a[0] < a[2])
+		sa(a, size);
+	else if (a[0] > a[1] && a[1] > a[2] && a[0] > a[2])
+	{
+		sa(a, size);
+		rra(s);
+	}
+	else if (a[0] > a[1] && a[1] < a[2] && a[0] > a[2])
+		ra(s);
+	else if (a[0] < a[1] && a[1] > a[2] && a[0] < a[2])
+	{
+		sa(a, size);
+		ra(s);
+	}
+	else
+		rra(s);
+}
 
 void exit_error(void)
 {
@@ -260,6 +285,33 @@ void	radix_sort(t_stack *a, t_stack *b, t_stacks *stacks)
 	}
 }
 
+void	push_min_b(t_stacks *stacks)
+{
+	int min_index;
+
+	min_index = get_min_index(stacks->a);
+	if (min_index > (int)((stacks->a->end - stacks->a->start) / 2))
+	{
+		while (min_index++ < stacks->a->end)
+			rra(stacks->a);
+	}
+	else
+	{
+		while (min_index-- > stacks->a->start)
+			ra(stacks->a);
+	}
+	pb(stacks->a, stacks->b);
+}
+
+void sort_5(t_stacks *stacks)
+{
+	push_min_b(stacks);
+	push_min_b(stacks);
+	three_sort(stacks->a);
+	print_stacks(stacks);
+	pa(stacks->a, stacks->b);
+	pa(stacks->a, stacks->b);
+}
 
 int main(int argc, char **argv)
 {
@@ -270,10 +322,12 @@ int main(int argc, char **argv)
 	check_input(argc, argv);
 	stacks = init_stacks(argc, argv);
 	fill_stack_a(stacks, argc, argv);
-	radix_sort(stacks->a, stacks->b, stacks);
+	sort_5(stacks);
+	//radix_sort(stacks->a, stacks->b, stacks);
 	//rb(stacks->b);
 	//pb(stacks->a, stacks->b);
 	//printf("%d", get_n_bit(5, 0));
+	print_stacks(stacks);
 	free_stacks(stacks);
 	return (0);
 }
